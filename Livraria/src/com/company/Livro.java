0a3f2a04860336333ffff851e3683570dfcdad39
ThiagoSulfa <thiagodebarrosgomes@gmail.com>
2020-03-17 21:25:18 -0300

    Vai filho

---
+++ Livraria/src/com/company/Livro.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Livro {
+
+    private String
+        titulo,
+        autor,
+        anoDeLancamento;
+
+    public double preco;
+
+    private int
+        codigo,
+        codigoISBN,
+        quantidadeEmEstoque,
+}
